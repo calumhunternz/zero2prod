fceba9e82409d02bb5b9e73250866c13e9d1335a
Calum <calum.hunter@canterbury.ac.nz>
2024-03-09 21:33:45 +1300

    refactor add tests and db

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,13 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
+use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8888")?
-    .run()
-    .await
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener = TcpListener::bind(address).expect("Failed to bind random port");
+    run(listener)?.await
 }
