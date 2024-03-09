204e7d723c235af489be6c4c998f2b7d9ddbce27
Calum <calum.hunter@canterbury.ac.nz>
2024-03-09 22:40:57 +1300

    added connection pool and updated tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -8,6 +9,10 @@ async fn main() -> Result<(), std::io::Error> {
 
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
