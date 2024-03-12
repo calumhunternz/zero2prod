eebd80e2f74c72a6e2eb80cf02ca76b6fb1e3e43
Calum <calum.hunter@canterbury.ac.nz>
2024-03-13 00:43:06 +1300

    refactored startup and integration tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,5 @@
-use sqlx::PgPool;
-use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::EmailClient;
-use zero2prod::startup::run;
+use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
@@ -12,23 +9,7 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-
-    let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
-
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, connection_pool, email_client)?.await
+    let server = Application::build(configuration.clone()).await?;
+    server.run_until_stopped().await?;
+    Ok(())
 }
