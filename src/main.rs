264169995bbaeb4f19025b5dcbc000906e48447c
Calum <calum.hunter@canterbury.ac.nz>
2024-03-12 17:46:51 +1300

    sent email_client to handle requests to postmark

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -11,6 +12,16 @@ async fn main() -> Result<(), std::io::Error> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
 
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email,
+        configuration.email_client.authorization_token,
+    );
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
@@ -19,5 +30,5 @@ async fn main() -> Result<(), std::io::Error> {
     let connection_pool = PgPool::connect_lazy_with(configuration.database.with_db());
 
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, connection_pool)?.await
+    run(listener, connection_pool, email_client)?.await
 }
