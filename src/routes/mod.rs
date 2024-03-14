3a3f9240f564aa1bd62fd3e68b22f638fb156d14
Calum <calum.hunter@canterbury.ac.nz>
2024-03-15 09:36:42 +1300

    added confirm feature (completed chapter 7)

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,6 @@
 mod health_check;
+mod subscription_confirm;
 mod subscriptions;
 pub use health_check::*;
+pub use subscription_confirm::*;
 pub use subscriptions::*;
--- src/startup.rs
+++ src/startup.rs
@@ -8,7 +8,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 
 pub struct Application {
     port: u16,
@@ -37,7 +37,12 @@ impl Application {
         );
         let listener = TcpListener::bind(address)?;
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, connection_pool, email_client)?;
+        let server = run(
+            listener,
+            connection_pool,
+            email_client,
+            configuration.application.base_url,
+        )?;
         // We "save" the bound port in one of `Application`'s fields
         Ok(Self { port, server })
     }
@@ -52,48 +57,29 @@ impl Application {
     }
 }
 
-pub async fn build(configuration: Settings) -> Result<Server, std::io::Error> {
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
-
-    let address = format!(
-        "{}:{}",
-        configuration.application.host, configuration.application.port
-    );
-
-    let connection_pool = get_connection_pool(&configuration.database);
-
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, connection_pool, email_client)
-}
-
 pub fn get_connection_pool(configuration: &DatabaseSettings) -> PgPool {
     PgPoolOptions::new().connect_lazy_with(configuration.with_db())
 }
+
+pub struct ApplicationBaseUrl(pub String);
 
 pub fn run(
     listener: TcpListener,
     db_pool: PgPool,
     email_client: EmailClient,
+    base_url: String,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
+    let base_url = Data::new(ApplicationBaseUrl(base_url));
 
     let server = HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
+            .app_data(base_url.clone())
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
